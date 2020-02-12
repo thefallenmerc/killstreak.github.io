d74e382c9502bf9d1c261f1d86a640eaf7db467e
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-02-13 00:39:49 +0530

    added build to git

---
+++ build/precache-manifest.9a4cad51398cd98dd194f02492352a2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675ad81c82f07f9473c12631faa5b08c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80ab8e17828da4cafd2c",
+    "url": "/static/css/main.8ee0fca4.chunk.css"
+  },
+  {
+    "revision": "857dec47e630496e5e94",
+    "url": "/static/js/2.d26ce672.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.d26ce672.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80ab8e17828da4cafd2c",
+    "url": "/static/js/main.c9577f5b.chunk.js"
+  },
+  {
+    "revision": "d17e73c5cabf9d64cbe2",
+    "url": "/static/js/runtime-main.35086dfe.js"
+  }
+]);
