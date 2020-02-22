50f56edc1f9ffae7c66aeebcd5748f5dcc16300f
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-02-22 18:05:50 +0530

    added gh-build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a4cad51398cd98dd194f02492352a2b.js"
+  "/precache-manifest.2fa94bea533db20a169e31944ccd98f1.js"
 );
 
 self.addEventListener('message', (event) => {
